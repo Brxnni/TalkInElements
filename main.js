12212f916eb0ba18ffea70fca8a6e5209afb5447
Brxnni <72916383+Brxnni@users.noreply.github.com>
2022-03-28 14:53:21 +0200

    Add spaces
    Spaces are cool, Zero-width spaces are not

--- main.js
+++ main.js
@@ -28,85 +28,104 @@ function generate(){
 
     // Read text from input
     text = document.getElementById("textInput").value.toLowerCase();
-
-    // Convert to list of element symbols
-    solutions = [];
-    recursiveSolve(text, []);
+    // Delete non-alphabetical characters
+    text = text.replace(/[^a-z ]/, "");
+    // Split by spaces
+    text = text.split(" ");
 
     div = document.getElementById("blocks");
-    
     // Remove already present blocks
     while (div.firstChild){ div.removeChild(div.firstChild); }
-    
-    // Check if a solution was even found
-    // If not, display error message
-    if (solutions.length == 0){
-        block = document.createElement("div")
-        block.innerHTML = "Not possible :(";
-        block.classList.add("blockerror");
 
-        block.style.backgroundColor = "#ED0000";
-        div.appendChild(block);
-    }
+    for (word of text){
+        console.log(word);
 
-    // If so, Add symbols
-    else {
-        for (symbol of solutions[0]){
-            // Create element
+        // Convert to list of element symbols
+        solutions = [];
+        recursiveSolve(word, []);
+        
+        // Check if a solution was even found
+        // If not, display error message
+        if (solutions.length == 0){
+            while (div.firstChild){ div.removeChild(div.firstChild); }
+
             block = document.createElement("div")
-            childDiv1 = document.createElement("div")
-            childDiv1.innerHTML = symbol.charAt(0).toUpperCase() + symbol.slice(1);
-            block.appendChild(childDiv1);
-            block.classList.add("block");
+            block.innerHTML = "Not possible :(";
+            block.classList.add("blockerror");
 
-            let colorString = "#ffffff";
-            // Choose color for block based on chemical group
-            // Non-metals
-            if (["h","c","n","o","p","s","se"].includes(symbol)){
-                colorString = "#20C8D3";
-            // Halogens
-            } else if (["f","cl","br","i","at","ts"].includes(symbol)){
-                colorString = "#9FBF35";
-            // Noble gases
-            } else if (["he","ne","ar","kr","xe","rn","og"].includes(symbol)){
-                colorString = "#30C337";
-            // Metalloids
-            } else if (["b","si","ge","as","sb","te"].includes(symbol)){
-                colorString = "#4F38E2";
-            // Post-transition metals (?)
-            } else if (["al","ga","in","sn","tl","pb","bi","po","nh","fl","mc","ts"].includes(symbol)){
-                colorString = "#C179C2";
-            // Transition metals
-            } else if (["sc","ti","v","cr","mn","fe","co","ni","cu","zn","y","zr","nb","mo","tc","ru","rh","pd","ag","cd","hf","ta","w","re","os","ir","pt","au","hg","rf","db","sg","bh","hs","mt","ds","rg","cn"].includes(symbol)){
-                colorString = "#ED1312";
-            // Alkali metals
-            } else if (["li","na","k","rb","cs","fr"].includes(symbol)){
-                colorString = "#EDEE16";
-            // Earth alkali metals
-            } else if (["be","mg","ca","st","ba","ra"].includes(symbol)){
-                colorString = "#ED7912";
-            // Lanthanides
-            } else if (["la","ce","pr","nd","pm","sm","eu","gd","tb","dy","ho","er","tm","yt","lu"].includes(symbol)){
-                colorString = "#F68B81";
-            // Actinides
-            } else if (["ac","th","pa","u","np","pu","am","cm","bk","cf","es","fm","md","no","lr"].includes(symbol)){
-                colorString = "#82DAE8";
-            }
-
-            // Create corner text
-            childDiv2 = document.createElement("div");
-            childDiv2.innerHTML = elements.indexOf(symbol) + 1;
-            childDiv2.classList.add("cornertext")
-            block.appendChild(childDiv2);
-
-            // Create bottom text
-            childDiv3 = document.createElement("div");
-            childDiv3.innerHTML = names[symbol];
-            childDiv3.classList.add("bottomtext")
-            block.appendChild(childDiv3);
-
-            block.style.backgroundColor = colorString;
-            div.appendChild(block)
+            block.style.backgroundColor = "#ED0000";
+            div.appendChild(block);
         }
+
+        // If so, Add symbols
+        else {
+            for (symbol of solutions[0]){
+                // Create element
+                block = document.createElement("div")
+                childDiv1 = document.createElement("div")
+                childDiv1.innerHTML = symbol.charAt(0).toUpperCase() + symbol.slice(1);
+                block.appendChild(childDiv1);
+                block.classList.add("block");
+
+                let colorString = "#ffffff";
+                // Choose color for block based on chemical group
+                // Non-metals
+                if (["h","c","n","o","p","s","se"].includes(symbol)){
+                    colorString = "#20C8D3";
+                // Halogens
+                } else if (["f","cl","br","i","at","ts"].includes(symbol)){
+                    colorString = "#9FBF35";
+                // Noble gases
+                } else if (["he","ne","ar","kr","xe","rn","og"].includes(symbol)){
+                    colorString = "#30C337";
+                // Metalloids
+                } else if (["b","si","ge","as","sb","te"].includes(symbol)){
+                    colorString = "#4F38E2";
+                // Post-transition metals (?)
+                } else if (["al","ga","in","sn","tl","pb","bi","po","nh","fl","mc","ts"].includes(symbol)){
+                    colorString = "#C179C2";
+                // Transition metals
+                } else if (["sc","ti","v","cr","mn","fe","co","ni","cu","zn","y","zr","nb","mo","tc","ru","rh","pd","ag","cd","hf","ta","w","re","os","ir","pt","au","hg","rf","db","sg","bh","hs","mt","ds","rg","cn"].includes(symbol)){
+                    colorString = "#ED1312";
+                // Alkali metals
+                } else if (["li","na","k","rb","cs","fr"].includes(symbol)){
+                    colorString = "#EDEE16";
+                // Earth alkali metals
+                } else if (["be","mg","ca","st","ba","ra"].includes(symbol)){
+                    colorString = "#ED7912";
+                // Lanthanides
+                } else if (["la","ce","pr","nd","pm","sm","eu","gd","tb","dy","ho","er","tm","yt","lu"].includes(symbol)){
+                    colorString = "#F68B81";
+                // Actinides
+                } else if (["ac","th","pa","u","np","pu","am","cm","bk","cf","es","fm","md","no","lr"].includes(symbol)){
+                    colorString = "#82DAE8";
+                }
+
+                // Create corner text
+                childDiv2 = document.createElement("div");
+                childDiv2.innerHTML = elements.indexOf(symbol) + 1;
+                childDiv2.classList.add("cornertext")
+                block.appendChild(childDiv2);
+
+                // Create bottom text
+                childDiv3 = document.createElement("div");
+                childDiv3.innerHTML = names[symbol];
+                childDiv3.classList.add("bottomtext")
+                block.appendChild(childDiv3);
+
+                block.style.backgroundColor = colorString;
+                div.appendChild(block)
+            }
+        }
+
+        block = document.createElement("div");
+        block.classList.add("block");
+        block.classList.add("space");
+        block.style.backgroundColor = "#252525";
+        div.appendChild(block);
+
     }
+
+    // Delete last pace
+    div.removeChild(div.lastChild);
 }
